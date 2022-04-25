922350d6996a7b674f2fb106461e8fd108ea939c
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-04-25 13:42:41 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+number_list = []
+new_number_list = []
+i = 0
+for i in range(0, 101):
+    if i % 5 == 0:
+        number_list.append(i)
+        new_number_list.append(i*i*i)
+        i = i + 1
+    else:
+        continue
+print(number_list)
+print(new_number_list)
