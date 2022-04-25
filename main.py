59a4ca92162fa7b46f9c3a142b035a65eadf0040
AgataNH <104362456+AgataNH@users.noreply.github.com>
2022-04-25 18:38:59 +0200

    test3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ for i in range(0, 101):
     if i % 5 == 0:
         number_list.append(i)
         new_number_list.append(i*i*i)
-        i = i + 1
+        i = i + 2
     else:
         continue
 print(number_list)
